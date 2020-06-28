b5cc0608f998099cb57bc0dee55e603bd8be7e89
erick <erick@MSI>
2020-06-28 17:31:26 -0600

    firs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Title from './components/title';
+import Input from './components/input';
+import Layout from './components/layout';
+import Button from './components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: "",
+      tasks: []
+    };
+  }
+
+  handleTaskChange = event => {
+    this.setState({
+      newTask: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let newTasks = this.state.tasks;
+    newTasks.push(this.state.newTask);
+    this.setState({
+      newTask: "",
+      tasks: newTasks
+    });
+  };
+
+  handleDelete = (id) => {
+    let newTasks = this.state.tasks;
+    newTasks.splice(id, 1)
+    this.setState({
+      tasks: newTasks,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Title/>
+        <Input />
+        <Button/>
+      </Layout>
+     
+    );
+  }
 }
 
 export default App;
---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+  
+import React from "react";
+
+function Button() {
+  return (
+    <form >
+          <button className="button-gen">prueba</button>
+    </form>
+  );
+}
+
+export default Button
